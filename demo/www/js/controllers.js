06f501cc94edc2d5b2dd85f5b7f09dd65436b9cf
songjiea <songjiea@yonyou.com>
2017-03-16 09:47:58 +0800

    删除model文件夹，修改获取模板方式

--- demo/www/js/controllers.js
+++ demo/www/js/controllers.js
@@ -37,43 +37,7 @@ angular.module('starter.controllers', [])
         $scope.modal = modal;
     });
 
-    // document.addEventListener('bluetoothprint.DataReceived', function (e) {
-    //     $rootScope.$broadcast('bluetoothprint.DataReceived', e.bluetoothPrintData);
-    // }, false);
-    // document.addEventListener('bluetoothprint.StatusReceived', function (e) {
-    //     $rootScope.$broadcast('bluetoothprint.StatusReceived', e.bluetoothPrintStatus);
-    // }, false);
-
     document.addEventListener("deviceready", function () {
-
-        // $scope.$on('bluetoothprint.DataReceived', function (e, data) {
-
-        //     $scope.data.pairedDevices = [];
-        //     var dataArray = data.split(",");
-        //     for (var i = 0; i < dataArray.length - 1; i++) {
-        //         var str = dataArray[i];
-        //         var name = str.substring(0, str.indexOf("&"));
-        //         var address = str.substring(str.indexOf("&") + 1);
-        //         $scope.data.pairedDevices.push({
-        //             name: name,
-        //             address: address
-        //         });
-        //     }
-
-        //     $scope.settingModal.show();
-
-        // });
-
-        // $scope.$on('bluetoothprint.StatusReceived', function (e, data) {
-        //     //连接成功，存入选中的
-        //     if (data === "true") {
-        //         if ($scope.data.selectDevice != null && $scope.data.selectDevice !== "") {
-        //             window.localStorage.setItem("gprinter", angular.toJson($scope.data.selectDevice));
-        //         }  
-        //     }
-        //     $ionicLoading.hide();
-        // });
-
         //如果有打印机地址，连接打印机
         var gprinter_address = angular.fromJson(window.localStorage.getItem("gprinter")) + "";   
         if (gprinter_address != null && gprinter_address !== "") {
@@ -84,76 +48,111 @@ angular.module('starter.controllers', [])
   
     //打印方法
     function printTag(){
-        getPrintString().then(function(str){
+        var str = getPrintString();
+        if (window.cordova && cordova.plugins.BluetoothPrint) {
             cordova.plugins.BluetoothPrint.printText(str, function (success) {
                 
             }, function (error) {
                 
             });
-        });
-            
+        }       
     }
+  
+    //定义标签纸宽高
+    function getSIZE(w,h){
+        return "SIZE "+w+" mm, "+h+" mm";
+    }
+    //间隙高度，间隙高度补偿值
+    function getGAP(m,n){
+        return "GAP "+m+" mm, "+n+" mm";
+    }
+    //打印速度
+    function getSPEED(s){
+        return "SPEED "+s;
+    }
+    //打印浓度
+    function getDENSITY(d){
+        return "DENSITY "+d;
+    }
+    //打印方向
+    function getDIRECTION(d){
+        return "DIRECTION "+d;
+    }
+    //清除缓存
+    function getCLS(){
+        return "CLS";
+    }
+    //设置文字
+    //X：文字方塊左上角 X 座标，Y: 文字方塊左上角 Y 座标，font：字型名称，
+    //rotation：顺时针旋转角度，X-multiplication：水平放大值（1-10），
+    //Y-multiplication：垂直放大值（1-10），content：文字内容字符串
+    function getTEXT(x,y,font,rotation,xm,ym,content){
+        return "TEXT "+x+","+y+","+"\""+font+"\""+","+rotation+","+xm+","+ym+","+"\""+content+"\"";
+    }
+    //设置二维码
+    //X：QRCODE 条码左上角 X 座标，Y：QRCODE 条码左上角 Y 座标，
+    //ECC level：错误纠正能力等级，cell width：1-10，mode：A/M(自动或手动生成编码)
+    //rotation：顺时针旋转角度，Data string：条码内容字符串
+    function getQRCODE(x,y,level,cw,mode,rotation,content){
+        return "QRCODE "+x+","+y+","+level+","+cw+","+mode+","+rotation+","+"\""+content+"\"";
+    }
+    //打印张数，打印张数的copy数
+    function getPRINT(m,n){
+        return "PRINT "+m+","+n;
+    }
+
+    function getModel1Str(){
+        return getSIZE(70,15)+"\n"
+               + getGAP(2,0)+"\n"
+               + getSPEED(4)+"\n"
+               + getDENSITY(8)+"\n"
+               + getDIRECTION(1)+"\n"
+               + getCLS()+"\n"
+               + getTEXT(20,30,3,0,1,1,$scope.data.chipType)+"\n"
+               + getTEXT(20,80,3,0,1,1,$scope.data.chipId)+"\n"
+               + getPRINT(1,1)+"\n";
+
+    }
+    function getModel2Str(){
+        return getSIZE(70,15)+"\n"
+               + getGAP(2,0)+"\n"
+               + getSPEED(4)+"\n"
+               + getDENSITY(8)+"\n"
+               + getDIRECTION(1)+"\n"
+               + getCLS()+"\n"
+               + getTEXT(20,30,3,0,1,1,$scope.data.chipType)+"\n"
+               + getQRCODE(100,15,"H",4,"A",0,$scope.data.chipId)+"\n"
+               + getPRINT(1,1)+"\n";
+
+    }
+    function getModel3Str(){
+        return getSIZE(70,15)+"\n"
+               + getGAP(2,0)+"\n"
+               + getSPEED(4)+"\n"
+               + getDENSITY(8)+"\n"
+               + getDIRECTION(1)+"\n"
+               + getCLS()+"\n"
+               + getQRCODE(100,15,"H",4,"A",0,$scope.data.chipId)+"\n"
+               + getPRINT(1,1)+"\n";
+
+    }
+
     //获取打印命令字符串
     function getPrintString(){
-        var deferred = $q.defer();
         var printString = '';
-        var jsondata = '';
         if($scope.data.selectModel === "model1"){
-            jsondata="model/model1.json"; 
-            $.getJSON(jsondata, function(data){ 
-                printString = data.SIZE+"\n"
-                      +data.GAP+"\n"
-                      +data.SPEED+"\n"
-                      +data.DENSITY+"\n"
-                      +data.DIRECTION+"\n"
-                      +data.CLS+"\n"
-                      +data.TEXT1+"\""+$scope.data.chipType+"\""+"\n"
-                      +data.TEXT2+"\""+$scope.data.chipId+"\""+"\n"
-                      +data.TEXT3+"\""+$scope.data.chipType+"\""+"\n"
-                      +data.TEXT4+"\""+$scope.data.chipType+"\""+"\n"
-                      +data.TEXT5+"\""+$scope.data.chipId+"\""+"\n"
-                      +data.TEXT6+"\""+$scope.data.chipId+"\""+"\n"
-                      +data.QRCODE+"\""+$scope.data.chipId+"\""+"\n"
-                      +data.TEXT7+"\""+$scope.data.chipId+"\""+"\n"
-                      +data.TEXT8+"\""+$scope.data.chipId+"\""+"\n"
-                      +data.PRINT+"\n";
-                deferred.resolve(printString);     
-            });   
+            printString = getModel1Str();        
         }
         else if($scope.data.selectModel === "model2"){
-            jsondata="model/model2.json"; 
-            $.getJSON(jsondata, function(data){ 
-                printString = data.SIZE+"\n"
-                      +data.GAP+"\n"
-                      +data.SPEED+"\n"
-                      +data.DENSITY+"\n"
-                      +data.DIRECTION+"\n"
-                      +data.CLS+"\n"
-                      +data.TEXT1+"\""+$scope.data.chipType+"\""+"\n"
-                      +data.QRCODE+"\""+$scope.data.chipId+"\""+"\n"
-                      +data.PRINT+"\n";
-                deferred.resolve(printString);
-            });  
+            printString = getModel2Str();  
         }
         else if($scope.data.selectModel === "model3"){
-            jsondata="model/model3.json"; 
-            $.getJSON(jsondata, function(data){ 
-                printString = data.SIZE+"\n"
-                      +data.GAP+"\n"
-                      +data.SPEED+"\n"
-                      +data.DENSITY+"\n"
-                      +data.DIRECTION+"\n"
-                      +data.CLS+"\n"
-                      +data.TEXT1+"\""+$scope.data.chipType+"\""+"\n"
-                      
-                      +data.PRINT+"\n";
-                deferred.resolve(printString);     
-            }); 
+            printString = getModel3Str();  
         }
         else{
 
         }
-        return deferred.promise;
+        return printString;
         
     }
 
@@ -167,11 +166,16 @@ angular.module('starter.controllers', [])
             if (window.cordova && cordova.plugins.BluetoothPrint) {
                 cordova.plugins.BluetoothPrint.connectDevice(address, function (success) {
                     //连接成功，存入选中的蓝牙地址
-                    // $ionicLoading.hide();
+                    // $timeout(function(){
+                    //     $ionicLoading.hide();
+                    // });
+                    // alert("success");
                     window.localStorage.setItem("gprinter_add", angular.toJson(address));
                      
                 }, function (error) {
-                    // $ionicLoading.hide();   
+                    // $timeout(function(){
+                    //     $ionicLoading.hide();
+                    // });     
                 });
             }
         }
